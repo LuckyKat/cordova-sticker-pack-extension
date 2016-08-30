58e0dd36380c03d9d189eaf53d7419b38924c437
Hernan Zhou <hernanzh@gmail.com>
2016-08-30 11:20:11 +0200

    remove xcode

--- hooks/stickers.js
+++ hooks/stickers.js
@@ -1,7 +1,7 @@
 // using error to see if this shows up in AB
 console.error("Running");
 
-var xcode = require('./xcode'),
+var //xcode = require('./xcode'),
     fs = require('fs'),
     path = require('path');
 
