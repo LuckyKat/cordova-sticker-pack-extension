206cde8b97937dfb8c9a0076696a4472ca687c15
Hernan Zhou <hernanzh@gmail.com>
2016-08-30 11:03:25 +0200

    plugin

---
+++ hooks/stickers.js
@@ -0,0 +1,10 @@
+// using error to see if this shows up in AB
+console.error("Running");
+
+var xcode = require('xcode'),
+    fs = require('fs'),
+    path = require('path');
+
+module.exports = function (context) {
+
+};
