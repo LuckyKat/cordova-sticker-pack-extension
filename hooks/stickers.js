cdcb56da9215af5acc2ce26ca84ae3578f53df91
Hernan Zhou <hernanzh@gmail.com>
2016-08-31 11:48:18 +0200

    parse xml

--- hooks/stickers.js
+++ hooks/stickers.js
@@ -20,7 +20,7 @@ module.exports = function (context) {
         contents = contents.substring(contents.indexOf('<'));
     }
     var elementTree = context.requireCordovaModule('elementtree');
-    var bundleId = et.XML(contents).findall('widget')[0].get('id');
+    var bundleId = elementTree.XML(contents).findall('widget')[0].get('id');
     console.error('bundle id:', bundleId);
 
     var iosFolder = context.opts.cordova.project ? context.opts.cordova.project.root : path.join(context.opts.projectRoot, 'platforms/ios/');
